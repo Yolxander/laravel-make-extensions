4d993dba7f820d705abde1b326a875707413b1a2
Yolxanderhc <yolxander.jaca@humancontact.com>
2025-03-04 20:21:02 -0500

    updated all stubs

--- src/Console/Commands/MakeIntegrationTest.php
+++ src/Console/Commands/MakeIntegrationTest.php
@@ -15,7 +15,7 @@ class MakeIntegrationTest extends GeneratorCommand
 
   protected function getStub()
   {
-    return __DIR__ . '/../../../stubs/integration-test.stub';
+    return __DIR__ . '/../../integration-test.stub';
   }
 
   /**
